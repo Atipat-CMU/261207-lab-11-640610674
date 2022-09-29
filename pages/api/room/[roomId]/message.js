61cbb6c0de61357eccabd184d9eced5775bd9f14
Atipat-CMU <atipat_d@cmu.ac.th>
2022-09-29 15:03:42 +0700

    GET message completed

--- pages/api/room/[roomId]/message.js
+++ pages/api/room/[roomId]/message.js
@@ -8,6 +8,13 @@ import { checkToken } from "../../../../backendLibs/checkToken";
 export default function roomIdMessageRoute(req, res) {
   if (req.method === "GET") {
     //check token
+    const user = checkToken(req);
+    if (!user) {
+      return res.status(401).json({
+        ok: true,
+        message: "Yon don't permission to access this api",
+      });
+    }
 
     //get roomId from url
     const roomId = req.query.roomId;
@@ -15,8 +22,12 @@ export default function roomIdMessageRoute(req, res) {
     const rooms = readChatRoomsDB();
 
     //check if roomId exist
-
+    const roomTarget = rooms.find((x) => x.roomId === roomId);
+    if (!roomTarget) {
+      return res.status(404).json({ ok: false, message: "Invalid room id" });
+    }
     //find room and return
+    return res.json({ ok: true, messages: roomTarget.messages });
     //...
   } else if (req.method === "POST") {
     //check token
